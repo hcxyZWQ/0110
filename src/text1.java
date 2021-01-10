81e7927dfc76152fc7d81e7ad7212d56b7b2755c
4343 <2360703200@qq.com>
2021-01-10 10:55:30 +0800

    参数传值

---
+++ src/text1.java
@@ -0,0 +1,12 @@
+public class text1 {
+    public static void main(String[] args) {
+        int a;
+        a=3;
+        m1(a);
+        System.out.println("aaaaaaaaaaa:"+a);//参数传值
+    }
+    public static void m1(int a){
+        a=5;
+        System.out.println("ccccccccccccc:"+a);
+    }
+}
